db547a58d5efdda671771ad6133beab4cdd5c680
ugjka <esesmu@protonmail.com>
2020-04-04 00:22:21 +0300

    us states

--- trigger_covid.go
+++ trigger_covid.go
@@ -4,12 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"strings"
 
 	hbot "github.com/ugjka/hellabot"
 	log "gopkg.in/inconshreveable/log15.v2"
 )
 
-var covidTriggerReg = regexp.MustCompile(`(?i)\s*!+(?:covid-?(?:19)?|corona(?:virus)?)\s+(\w+.*)`)
+var covidTriggerReg = regexp.MustCompile(`(?i)\s*!+(?:covid-?(?:19)?|corona(?:virus|chan)?)\s+(\w+.*)`)
 var covidTrigger = hbot.Trigger{
 	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
 		return covidTriggerReg.MatchString(m.Content)
@@ -28,12 +29,30 @@ var covidTrigger = hbot.Trigger{
 			log.Error("covid", "decode error", err)
 			return false
 		}
-		irc.Reply(m, c.String())
+		states := make(states, 0)
+		state := state{}
+		resp, err = httpClient.Get(coronaStatesAPI)
+		if err == nil {
+			defer resp.Body.Close()
+			err = json.NewDecoder(resp.Body).Decode(&states)
+			if err == nil {
+				state = states.Search(country)
+			}
+		}
+		switch {
+		case state.State != "" && c.IsEmpty():
+			irc.Reply(m, state.String())
+		case state.State != "" && !c.IsEmpty():
+			irc.Reply(m, c.String())
+			irc.Reply(m, state.String())
+		case state.State == "":
+			irc.Reply(m, c.String())
+		}
 		return false
 	},
 }
 
-var covidAllTriggerReg = regexp.MustCompile(`(?i)^\s*!+(?:covid-?(?:19)?|corona(?:virus)?)$`)
+var covidAllTriggerReg = regexp.MustCompile(`(?i)^\s*!+(?:covid-?(?:19)?|corona(?:virus|chan)?)$`)
 var covidAllTrigger = hbot.Trigger{
 	Condition: func(bot *hbot.Bot, m *hbot.Message) bool {
 		return covidAllTriggerReg.MatchString(m.Content)
@@ -58,6 +77,7 @@ var covidAllTrigger = hbot.Trigger{
 
 var coronaCountryAPI = "https://corona.lmao.ninja/countries/"
 var coronaAllAPI = "https://corona.lmao.ninja/all"
+var coronaStatesAPI = "https://corona.lmao.ninja/states/"
 
 type covid struct {
 	Message     string
@@ -79,6 +99,10 @@ func (c covid) String() string {
 		c.Country, c.Cases, c.TodayCases, c.Deaths, c.TodayDeaths, c.Recovered, c.Active, c.Critical)
 }
 
+func (c covid) IsEmpty() bool {
+	return c.Message != ""
+}
+
 type covidAll struct {
 	Cases             int
 	Deaths            int
@@ -90,4 +114,30 @@ type covidAll struct {
 func (c covidAll) String() string {
 	return fmt.Sprintf("[Global] cases: %d, deaths: %d, recovered: %d, active: %d, affected countries: %d",
 		c.Cases, c.Deaths, c.Recovered, c.Active, c.AffectedCountries)
+}
+
+type state struct {
+	State       string
+	Cases       int
+	TodayCases  int
+	Deaths      int
+	TodayDeaths int
+	Active      int
+}
+
+type states []state
+
+func (s states) Search(q string) state {
+	q = strings.ToLower(q)
+	for _, v := range s {
+		if strings.ToLower(v.State) == q {
+			return v
+		}
+	}
+	return state{}
+}
+
+func (s *state) String() string {
+	return fmt.Sprintf("[USA, %s] cases: %d (+%d today), deaths %d (+%d today), active: %d",
+		s.State, s.Cases, s.TodayCases, s.Deaths, s.TodayDeaths, s.Active)
 }
