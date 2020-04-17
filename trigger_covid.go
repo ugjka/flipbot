48b810b97f70aa43f50f53e998abb5f29976432f
ugjka <esesmu@protonmail.com>
2020-04-17 21:04:05 +0300

    show local time in updated date

--- trigger_covid.go
+++ trigger_covid.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
 	hbot "github.com/ugjka/hellabot"
 	log "gopkg.in/inconshreveable/log15.v2"
+	"gopkg.in/ugjka/go-tz.v2/tz"
 )
 
 var covidTriggerReg = regexp.MustCompile(`(?i)\s*!+(?:covid-?(?:19)?|corona(?:virus|chan)?)\s+(\w+.*)`)
@@ -98,8 +102,12 @@ func (c covid) String() string {
 	if c.Message != "" {
 		return c.Message
 	}
+	updated := time.Unix(c.Updated/1000, 0).UTC()
+	if zone, err := getZone(c.Country); err == nil {
+		updated = updated.In(zone)
+	}
 	return fmt.Sprintf("[%s] [%s] cases: %d (+%d today), deaths: %d (+%d today), recovered: %d, active: %d, critical: %d, tests: %d",
-		time.Unix(c.Updated/1000, 0).UTC().Format("02/01/06 15:04 MST"), c.Country, c.Cases, c.TodayCases, c.Deaths, c.TodayDeaths, c.Recovered, c.Active, c.Critical, c.Tests)
+		updated.Format("02/01/06 15:04 MST"), c.Country, c.Cases, c.TodayCases, c.Deaths, c.TodayDeaths, c.Recovered, c.Active, c.Critical, c.Tests)
 }
 
 func (c covid) IsEmpty() bool {
@@ -149,4 +157,32 @@ func (s states) Search(q string) state {
 func (s *state) String() string {
 	return fmt.Sprintf("[USA, %s] cases: %d (+%d today), deaths %d (+%d today), active: %d, tests: %d",
 		s.State, s.Cases, s.TodayCases, s.Deaths, s.TodayDeaths, s.Active, s.Tests)
+}
+
+func getZone(loc string) (*time.Location, error) {
+	maps := url.Values{}
+	maps.Add("q", loc)
+	maps.Add("format", "json")
+	maps.Add("accept-language", "en")
+	maps.Add("limit", "1")
+	maps.Add("email", email)
+	data, err := OSMGetter(OSMGeocode + maps.Encode())
+	if err != nil {
+		return nil, err
+	}
+	var mapj OSMmapResults
+	if err = json.Unmarshal(data, &mapj); err != nil {
+		return nil, err
+	}
+	if len(mapj) == 0 {
+		return nil, errors.New("no place")
+	}
+	lat, _ := strconv.ParseFloat(mapj[0].Lat, 64)
+	lon, _ := strconv.ParseFloat(mapj[0].Lon, 64)
+	p := tz.Point{Lat: lat, Lon: lon}
+	tzid, err := tz.GetZone(p)
+	if err != nil {
+		return nil, err
+	}
+	return time.LoadLocation(tzid[0])
 }
