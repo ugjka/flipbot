703ef3daf306c80596772903ee86cef00c8f4209
ugjka <esesmu@protonmail.com>
2020-06-16 23:12:12 +0300

    add htop to the ami

--- packer/setup.sh
+++ packer/setup.sh
@@ -3,7 +3,7 @@ sudo mv /home/arch/flipbot.service /etc/systemd/system/
 sudo mv /home/arch/flipbotreload.service /etc/systemd/system/
 mkdir /home/arch/flipbot
 sudo systemctl enable flipbot.service
-sudo pacman -Syu --noconfirm yay bash-completion ncdu nano git wget mosh curl iperf3
+sudo pacman -Syu --noconfirm yay bash-completion ncdu nano git wget mosh curl iperf3 htop
 yay -S --noconfirm translate-shell googler codedeploy-agent
 sudo systemctl enable codedeploy-agent.service
 echo "UUID=83282862-5fc8-4e9a-be23-03840738cb2d       /home/arch/flipbot      ext4    rw,relatime     0 2" | sudo tee -a /etc/fstab
