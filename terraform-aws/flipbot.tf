f69bdef9c60ce03fed749a81ee616c88e0accd9c
ugjka <esesmu@protonmail.com>
2020-06-16 20:32:04 +0300

    devops

--- terraform-aws/flipbot.tf
+++ terraform-aws/flipbot.tf
@@ -122,7 +122,7 @@ resource "aws_volume_attachment" "fl_data_att" {
 }
 
 resource "aws_instance" "flipbot" {
-  ami           = "ami-080a54665e2d19667"
+  ami           = "ami-0861064668bc5bb55"
   instance_type = "t2.micro"
   availability_zone = "us-east-1a"
   key_name = aws_key_pair.default.key_name
