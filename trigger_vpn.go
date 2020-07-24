8b49715e78926867ae8ca65a0bb549ace9a3756e
ugjka <esesmu@protonmail.com>
2020-07-24 13:41:36 +0300

    better vpn check

--- trigger_vpn.go
+++ trigger_vpn.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os/exec"
 	"regexp"
 	"strconv"
@@ -29,7 +28,7 @@ var vpnTrigger = hbot.Trigger{
 			if !ipReg.MatchString(m.Host) {
 				return false
 			}
-			if m.Name == "klimdaddie" || m.Name == "madk" || m.Name == "fl1pbot" || strings.HasPrefix(m.Name, "algernon") {
+			if m.Name == "klimdaddie" || m.Name == ircNick {
 				return false
 			}
 			if len(m.Params) == 3 && m.Params[1] != "*" {
@@ -41,23 +40,23 @@ var vpnTrigger = hbot.Trigger{
 	Action: func(irc *hbot.Bot, m *hbot.Message) bool {
 		arr := ipReg.FindStringSubmatch(m.Host)
 		ip := arr[1]
-		vpn, err := whoisVPNCheck(ip)
+		vpn, err := subnetVPNCheck(ip)
 		if err != nil {
-			log.Error("whois vpn check", "error", err)
+			log.Error("subnet vpn check", "error", err)
 			return false
 		}
 		if vpn {
-			log.Info("whois vpn detected", "kicking", fmt.Sprintf("%s!%s@%s", m.Name, m.User, m.Host))
+			log.Info("subnet vpn detected", "kicking", fmt.Sprintf("%s!%s@%s", m.Name, m.User, m.Host))
 			irc.Send(fmt.Sprintf("REMOVE %s %s :VPN detected, please identify before joining to bypass this check", ircChannel, m.Name))
 			return false
 		}
-		vpn, err = torrentVPNCheck(ip)
+		vpn, err = providerVPNCheck(ip)
 		if err != nil {
-			log.Error("torrent vpn check", "error", err)
+			log.Error("provider vpn check", "error", err)
 			return false
 		}
 		if vpn {
-			log.Info("torrent vpn detected", "kicking", fmt.Sprintf("%s!%s@%s", m.Name, m.User, m.Host))
+			log.Info("provider vpn detected", "kicking", fmt.Sprintf("%s!%s@%s", m.Name, m.User, m.Host))
 			irc.Send(fmt.Sprintf("REMOVE %s %s :VPN detected, please identify before joining to bypass this check", ircChannel, m.Name))
 			return false
 		}
@@ -65,11 +64,8 @@ var vpnTrigger = hbot.Trigger{
 	},
 }
 
-var ipRangeReg = regexp.MustCompile(`(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
-
-const torrentThresholdSize = 1024 * 30
-
-func whoisVPNCheck(ip string) (vpn bool, err error) {
+func subnetVPNCheck(ip string) (vpn bool, err error) {
+	var ipRangeReg = regexp.MustCompile(`(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
 	cmd := exec.Command("whois", ip)
 	b, err := cmd.CombinedOutput()
 	if err != nil {
@@ -79,7 +75,8 @@ func whoisVPNCheck(ip string) (vpn bool, err error) {
 	if !ipRangeReg.MatchString(data) {
 		return false, fmt.Errorf("no range found")
 	}
-	arr := ipRangeReg.FindStringSubmatch(data)
+	res := ipRangeReg.FindAllStringSubmatch(data, -1)
+	arr := res[len(res)-1]
 	start := strings.Split(arr[1], ".")
 	end := strings.Split(arr[2], ".")
 	for i := 0; i < 2; i++ {
@@ -95,18 +92,22 @@ func whoisVPNCheck(ip string) (vpn bool, err error) {
 	return true, nil
 }
 
-func torrentVPNCheck(ip string) (vpn bool, err error) {
-	res, err := httpClient.Get("https://iknowwhatyoudownload.com/en/peer/?ip=" + ip)
+func providerVPNCheck(ip string) (vpn bool, err error) {
+	var providerDenylist = []string{
+		"abuse@m247.ro",
+		"abuse@cdn77.com",
+		"abuse@creanova.org",
+	}
+	cmd := exec.Command("whois", ip)
+	b, err := cmd.CombinedOutput()
 	if err != nil {
 		return
 	}
-	defer res.Body.Close()
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return
+	data := string(b)
+	for _, v := range providerDenylist {
+		if strings.Contains(data, v) {
+			return true, nil
+		}
 	}
-	if len(b) > torrentThresholdSize {
-		return true, nil
-	}
-	return
+	return false, nil
 }
