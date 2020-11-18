b14b36fa630450aea4a9c9549abb7ef2f8a94f3b
ugjka <esesmu@protonmail.com>
2020-11-18 21:43:42 +0200

    cleanup

--- bolt.go
+++ bolt.go
@@ -3,39 +3,16 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"regexp"
-	"strings"
-	"time"
 
 	"github.com/boltdb/bolt"
-	"mvdan.cc/xurls/v2"
 )
 
 var (
-	seenBucket     = []byte("seen")
 	osmCacheBucket = []byte("osmcache")
-	memoBucket     = []byte("memo")
-	reminderBucket = []byte("reminder")
-	logBucket      = []byte("log")
-	indexBucket    = []byte("index")
-	usersBucket    = []byte("users")
-	rankBucket     = []byte("ranks")
-	hostmaskBucket = []byte("hostmask")
-	quietBucket    = []byte("quiet")
 )
 
 func initDB(file string) (*bolt.DB, error) {
 	db, err := bolt.Open(file, 0600, nil)
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(seenBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return nil
-	})
 	if err != nil {
 		return nil, err
 	}
@@ -49,98 +26,7 @@ func initDB(file string) (*bolt.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(memoBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(reminderBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(logBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(indexBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(usersBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(rankBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return err
-	})
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(hostmaskBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(quietBucket)
-		if err != nil {
-			return fmt.Errorf("create bucket: %v", err)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
 	return db, nil
-}
-
-type dead struct {
-	times []struct {
-		duration time.Duration
-		posts    int
-	}
-	last struct {
-		seen time.Time
-		nick string
-	}
 }
 
 func setOSMCache(url string, data []byte) error {
@@ -168,15 +54,4 @@ func getOSMCache(url string) ([]byte, error) {
 		return nil, err
 	}
 	return buf.Bytes(), nil
-}
-
-var urls = xurls.Relaxed()
-var word = regexp.MustCompile("(!?\\w+[\"']?\\w+)")
-
-func split(in string) (out []string) {
-	if urls.MatchString(in) {
-		in = urls.ReplaceAllString(in, "")
-	}
-	in = strings.Replace(in, "’", "'", -1)
-	return word.FindAllString(in, -1)
 }
