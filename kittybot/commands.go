ec0f9e0119460270f5e7a0b6731b0256f7200ea5
ugjka <esesmu@protonmail.com>
2020-11-18 22:03:06 +0200

    embed

--- kittybot/commands.go
+++ kittybot/commands.go
@@ -1,6 +1,18 @@
 package kitty
 
+import (
+	"github.com/bwmarrin/discordgo"
+	"mvdan.cc/xurls/v2"
+)
+
 // Reply sends a message to where the message came from (user or channel)
 func (bot *Bot) Reply(m *Message, text string) {
-	m.Session.ChannelMessageSend(m.To, text)
+	urls := xurls.Strict().FindStringSubmatch(text)
+	if len(urls) == 0 {
+		m.Session.ChannelMessageSend(m.To, text)
+	}
+	m.Session.ChannelMessageSendEmbed(m.To, &discordgo.MessageEmbed{
+		URL:         urls[0],
+		Description: text,
+	})
 }
