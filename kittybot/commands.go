efd287719fc32f236397a404adf356f1aa572a41
ugjka <esesmu@protonmail.com>
2020-11-22 00:05:48 +0200

    log

--- kittybot/commands.go
+++ kittybot/commands.go
@@ -2,5 +2,6 @@ package kitty
 
 // Reply sends a message to where the message came from (user or channel)
 func (bot *Bot) Reply(m *Message, text string) {
+	bot.Info("Discord Chan", m.To)
 	m.Session.ChannelMessageSend(m.To, text)
 }
