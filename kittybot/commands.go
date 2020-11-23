50fefb74fbb25eaae771de976d2086a307262720
ugjka <esesmu@protonmail.com>
2020-11-23 11:19:19 +0200

    rename music icon

--- kittybot/commands.go
+++ kittybot/commands.go
@@ -30,9 +30,10 @@ func (bot *Bot) ReplyRich(m *Message, r Rich) {
 		Title:       r.Title,
 		Description: r.Description,
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
-			URL:    r.IconURL,
-			Width:  40,
-			Height: 40,
+			URL:      r.IconURL,
+			ProxyURL: r.URL,
+			Width:    40,
+			Height:   40,
 		},
 	})
 }
