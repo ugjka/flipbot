99ec5651bb9f426beb8403836cf3466c53efef3e
ugjka <esesmu@protonmail.com>
2020-11-24 20:17:07 +0200

    remove space cleaner

--- trigger_youtube.go
+++ trigger_youtube.go
@@ -42,20 +42,6 @@ var youtube = kitty.Trigger{
 		)
 		result = html.UnescapeString(result)
 		bot.Reply(m, result)
-
-		bytes, err := freeSpace(mp3Dir)
-		if err != nil {
-			bot.Error("df", "error", err)
-			return
-		}
-		if bytes < 1024*1024*2 {
-			err := emptyDir(mp3Dir)
-			if err != nil {
-				bot.Error("rm", "error", err)
-				return
-			}
-		}
-
 		// Fetch mp3
 		video := ytdlOptions{
 			url:           "https://www.youtube.com/watch?v=" + res.Items[0].ID.VideoID,
